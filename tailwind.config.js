30e1c88448b4c085284da04435f61b90cb3c68af
MikeProduce <Gomez_miguelomar@yahoo.com>
2023-06-24 21:08:50 -0500

    added some classes will fix the positioning later

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,25 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        darkBlueIntro: "hsl(217, 28%, 15%)",
+        darkBlueMain: "hsl(218, 28%, 13%)",
+        darkBlueFooter: "hsl(216, 53%, 9%)",
+        darkBlueTestimonials: "hsl(219, 30%, 18%)",
+        cyan: "hsl(176, 68%, 64%)",
+        blue: "hsl(198, 60%, 50%)",
+        lightRed: "hsl(0, 100%, 63%)",
+        white: "hsl(0, 0%, 100%)",
+      },
+      fontSize: {
+        '14': '14px',
+      },
+      fontFamily: {
+        sans: ['Open Sans', 'sans-serif'],
+        raleway: ['Raleway', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
-}
+};
